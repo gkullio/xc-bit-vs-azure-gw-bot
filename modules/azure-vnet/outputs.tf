cdd19ac79cc5279fc6dffed29db12adeed9a2bac
gkullio <105679126+gkullio@users.noreply.github.com>
2025-03-21 17:52:12 -0700

    Update outputs.tf

--- modules/azure-vnet/outputs.tf
+++ modules/azure-vnet/outputs.tf
@@ -2,14 +2,6 @@ output "vnet_id" {
   description   = "The ID of the virtual network"
   value         = azurerm_virtual_network.vnet.id
 }
-output "mgmt_subnet_id" {
-  description   = "The ID of the management subnet"
-  value         = azurerm_subnet.mgmt.id
-}
-output "int_subnet_id" {
-  description   = "The ID of the internal subnet"
-  value         = azurerm_subnet.int.id
-}
 output "ext_subnet_id" {
   description   = "The ID of the external subnet"
   value         = azurerm_subnet.ext.id
